5f561e449d3447a92ab82e11c59fbf64ae6da785
fito <fifodominguez@gmail.com>
2019-05-02 22:09:51 -0600

    Updates

---
+++ precache-manifest.cbac81f791b70a9749a1abd50b8aa071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3937738cf1269fde3d68",
+    "url": "/gastosemanal/static/css/main.ecae3b4c.chunk.css"
+  },
+  {
+    "revision": "3937738cf1269fde3d68",
+    "url": "/gastosemanal/static/js/main.2a941fb8.chunk.js"
+  },
+  {
+    "revision": "8b6a4e098b7006de9a65",
+    "url": "/gastosemanal/static/js/runtime~main.948489c5.js"
+  },
+  {
+    "revision": "08e6dcb1f224a455d76e",
+    "url": "/gastosemanal/static/js/2.1fccb98a.chunk.js"
+  },
+  {
+    "revision": "39e78100fb7123d330e136e53b643c56",
+    "url": "/gastosemanal/index.html"
+  }
+];
